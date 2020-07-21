6829101c54c293a2678b68fa1472e22e95a745f1
VaughnFisher <vaughngfisher@gmail.com>
2020-07-21 16:42:01 -0700

    completed anecdotes 1.11-1.14

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom'
 
 const Heading = (props) => <h1>{props.title}</h1>
 
+const Anecdote = (props) => (
+  <>
+    <p>"{anecdotes[props.anecdote]}"</p>
+    <p>has {props.votes[props.anecdote]} votes</p>
+  </>
+)
+
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
@@ -10,7 +17,10 @@ const Button = (props) => (
 )
 
 const Stat = (props) => (
-  <p>{props.text} {props.value}</p>
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </tr>
 )
 
 const Stats = ({data}) => {
@@ -18,25 +28,49 @@ const Stats = ({data}) => {
   const average = (data.good - data.bad)/total
   const positive = 100 * (data.good/total) + ' %'
 
-  console.log(data.good)
-
-
   if(total===0){
     return (
       <>
       <h1>Statistics</h1>
-      <p>No Feedback Given</p>
+      <p>No feedback given</p>
       </>
     )
   }
   return (
     <>
-    <h1>Statistics</h1>
-    <Stat text="Good:" value={data.good} />
-    <Stat text="Neutral:" value={data.neutral} />
-    <Stat text="Bad" value={data.bad} />
-    <Stat text="Average" value={average} />
-    <Stat text="Positive" value={positive} />
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <Stat text="Good: " value={data.good} />
+          <Stat text="Neutral: " value={data.neutral} />
+          <Stat text="Bad: " value={data.bad} />
+          <Stat text="Average: " value={average} />
+          <Stat text="Positive: " value={positive} />
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+
+
+const MostVotes = (props) => {
+  const mostVotes = Math.max(...props.votes)
+  const index = props.votes.indexOf(mostVotes)
+
+  if(mostVotes > 0){
+    return (
+      <>
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={index} votes={props.votes} />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>Anecdote with most votes</h1>
+      <p>No votes for anecdotes submitted</p>
     </>
   )
 }
@@ -46,13 +80,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const data = {
     good: good,
     neutral: neutral,
     bad: bad
   }
-
 
   const title = "Give Feedback!"
 
@@ -62,7 +97,23 @@ const App = () => {
 
   const incBad = newValue => setBad(newValue)
 
+  const randAnec = newValue => setSelected(newValue)
+
+  const addVote = (selected) => {
+    const newVotes = [...votes] 
+    newVotes[selected]++
+    setVotes(newVotes)
+  }
+
   return (
+    <>
+    <div>
+      <Heading title="Anecdote of the day" />
+      <Anecdote anecdote={selected} votes={votes} />
+      <Button handleClick={() => addVote(selected)} text="Vote"/>
+      <Button handleClick={() => randAnec(Math.floor(Math.random()*6))} text="New Anecdote" />
+      <MostVotes anecdotes={anecdotes} votes={votes}/>
+    </div>
     <div>
       <Heading title={title} />
       <Button handleClick={() => incGood(good+1)} text="Good" />
@@ -70,8 +121,19 @@ const App = () => {
       <Button handleClick={() => incBad(bad+1)} text="Bad" />
       <Stats data={data} />
     </div>
+    </>
   )
 }
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
