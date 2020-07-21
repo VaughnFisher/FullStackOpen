b8ac8c69a73970d2947682c00930648f50979323
VaughnFisher <vaughngfisher@gmail.com>
2020-07-21 01:35:08 -0700

    completed 1.6-1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Heading = (props) => <h1>{props.title}</h1>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Stat = (props) => (
+  <p>{props.text} {props.value}</p>
+)
+
+const Stats = ({data}) => {
+  const total = data.good + data.neutral + data.bad
+  const average = (data.good - data.bad)/total
+  const positive = 100 * (data.good/total) + ' %'
+
+  console.log(data.good)
+
+
+  if(total===0){
+    return (
+      <>
+      <h1>Statistics</h1>
+      <p>No Feedback Given</p>
+      </>
+    )
+  }
+  return (
+    <>
+    <h1>Statistics</h1>
+    <Stat text="Good:" value={data.good} />
+    <Stat text="Neutral:" value={data.neutral} />
+    <Stat text="Bad" value={data.bad} />
+    <Stat text="Average" value={average} />
+    <Stat text="Positive" value={positive} />
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const data = {
+    good: good,
+    neutral: neutral,
+    bad: bad
+  }
+
+
+  const title = "Give Feedback!"
+
+  const incGood = newValue => setGood(newValue)
+
+  const incNeutral = newValue => setNeutral(newValue)
+
+  const incBad = newValue => setBad(newValue)
+
+  return (
+    <div>
+      <Heading title={title} />
+      <Button handleClick={() => incGood(good+1)} text="Good" />
+      <Button handleClick={() => incNeutral(neutral+1)} text="Neutral" />
+      <Button handleClick={() => incBad(bad+1)} text="Bad" />
+      <Stats data={data} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
